7d237eec702985c04033e990ebdd606aadbccbf1
gaohuang <2933682586@qq.com>
2016-08-08 13:05:13 +0800

    modify

--- app/src/main/java/com/example/gaohuang/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/gaohuang/fragmenttest/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.gaohuang.fragmenttest;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +15,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ThirdFragment fragment = new ThirdFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.framelayout, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
     }
 }
